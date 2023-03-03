6e264c5610cf01fdc255c7e6f4ac3c5ff2e5341c
Greg Kovshov <barkev2009@bk.ru>
2023-03-04 04:49:00 +0700

    minor fix

--- client/src/index.js
+++ client/src/index.js
@@ -17,10 +17,8 @@ const store = configureStore({
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
 );
 
