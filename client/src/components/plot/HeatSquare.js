712de342429d6e1f5365fdd7a688fccca8cb22f6
Greg Kovshov <barkev2009@bk.ru>
2023-03-13 19:55:51 +0700

    added palletes highlight

--- client/src/components/plot/HeatSquare.js
+++ client/src/components/plot/HeatSquare.js
@@ -1,8 +1,13 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setChosenMeasure } from '../../redux/tableSlice';
 
 const HeatSquare = ({ rawItem }) => {
 
-    const [text, setText] = useState('')
+    const [text, setText] = useState('');
+
+    const chosenMeasure = useSelector(state => state.measures.chosenMeasure);
+    const dispatch = useDispatch();
 
     const colorHandler = () => {
         if (rawItem.ph_level >= 7 && rawItem.ph_level <= 7.2) {
@@ -14,8 +19,20 @@ const HeatSquare = ({ rawItem }) => {
         }
     }
 
+    const activeHandler = () => {
+        return chosenMeasure === rawItem.id ? 'active' : ''
+    }
+
+    const clickHandler = () => {
+        if (chosenMeasure === rawItem.id) {
+            dispatch(setChosenMeasure(null))
+        } else {
+            dispatch(setChosenMeasure(rawItem.id))
+        }
+    }
+
     return (
-        <div onMouseOver={() => setText(rawItem.ph_level)} onMouseLeave={() => setText('')} className='heat_square' style={{backgroundColor: colorHandler()}}>{text}</div>
+        <div onClick={clickHandler} onMouseOver={() => setText(rawItem.ph_level)} onMouseLeave={() => setText('')} className={`heat_square ${activeHandler()}`} style={{backgroundColor: colorHandler()}}>{text}</div>
     )
 }
 
